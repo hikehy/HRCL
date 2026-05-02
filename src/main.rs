b3c0413f8dc019728a5ac79fc2d1aaf241862c23
hikehy <hikey711@gmail.com>
2026-05-01 22:15:01 -0500

    this will most likely be my last commit on this project, it is done. I might come back to it but that is unlikely, Bye - Hikehy

--- src/main.rs
+++ src/main.rs
@@ -3,36 +3,50 @@ use std::fs;
 use std::io::Write;
 use std::process::exit;
 
-fn main() {
-    loop {
-        //Main input starting out
-        //Presenting where you are -> flushing before -> taking input -> further goes onto processing input
-        print!("HRCL$ ");
-        std::io::stdout().flush().expect("Could not flush stdout");
-        let mut input = String::new();
-        std::io::stdin().read_line(&mut input).unwrap();
-        if input.trim() == "exit" {
-            exit(0);
-        } else {
-            let _ = run_lua(input.trim());
-        }
-    }
+fn parse_args(input: &str) -> Vec<&str> {
+    input.split_whitespace().collect()
 }
 
-fn run_lua(command: &str) -> std::result::Result<(), Box<dyn std::error::Error>> {
+fn run_lua(command: &str, args: Vec<&str>) -> std::result::Result<(), Box<dyn std::error::Error>> {
     let filepath = format!("src/functions/{}.lua", command.trim());
     let script = fs::read_to_string(&filepath)?;
 
     let lua = mlua::Lua::new();
-    lua.load(&script).exec()?;
 
+    let lua_args = lua.create_table()?;
+    for (i, arg) in args.iter().enumerate() {
+        lua_args.set(i + 1, *arg)?;
+    }
+    lua.globals().set("args", lua_args)?;
+
+    lua.load(&script).exec()?;
     Ok(())
 }
 
-// Things i need to figure out: Executing funcions in other files
-//
-// Ill mainly do this through mod
-// the file structure will look like this: src/command/mod.rs, other files.rs
-// this will allow me to include the commands i want to add in mod.rs, and i should be able to include mod.rs in the main file to execute the functions in the custom
-// files. I also want the user to be able to add and rs files they want to, i plan on using an external script to do this so they dont have to recomplie the proj everytime
-// anyways its 3:45 am so ima call it. GN
+fn main() {
+    loop {
+        print!("HRCL$ ");
+        std::io::stdout().flush().expect("Could not flush stdout");
+
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+
+        let input = input.trim();
+
+        if input == "exit" {
+            exit(0);
+        }
+
+        let mut parts = parse_args(input);
+
+        if parts.is_empty() {
+            continue;
+        }
+
+        let command = parts.remove(0);
+
+        if let Err(e) = run_lua(command, parts) {
+            eprintln!("error: {}", e);
+        }
+    }
+}
